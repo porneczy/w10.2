e9e7660db272b2f8cf2911e43afb95beeb781342
porneczy <porneczy@gmail.com>
2022-04-05 21:20:07 +0200

    ujrakezdtem

---
+++ frontend/pub/script.js
@@ -0,0 +1,25 @@
+const parseJSON = async (url) => {
+    const response = await fetch(url)
+    return response.json()
+}
+
+//kapcsoszárojel, egy objectumból kivonjuk a kulcsokat  (object destructuring)
+const beerComponent = ({title, sub}) => {
+    return `
+    <div>
+        <h1>${title}</h1>
+        <h2>${sub}</h2>
+    </div>
+    `
+}
+
+const loadEvent = async () => {
+    const rootElement = document.getElementById("root");
+    const result = await parseJSON("/beers")
+    
+    rootElement.insertAdjacentHTML(
+        'beforeend',
+        result.cards.map( beer => beerComponent(beer) ).join("")
+    )
+}
+window.addEventListener("load", loadEvent);
