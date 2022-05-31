66bbfaee163c6d12a348b9658d02e2f4ddcff457
porneczy <porneczy@gmail.com>
2022-05-31 21:08:52 +0200

    .

--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -1,6 +1,60 @@
-const parseJSON = async (url) => {
-    const response = await fetch(url)
-    return response.json()
+
+const beers = {
+    "id": "beers",
+    "logo": "Best Beers",
+    "button": "details",
+    "cards": [
+        {
+            "title": "Mango Bay",
+            "sub": "Mad Scientist Beer",
+            "text": "Pale Ale - American"
+        },
+        {
+            "title": "Távoli Galaxis",
+            "sub": "Rothbeer Brewery",
+            "text": "IPA - American"
+        },
+        {
+            "title": "Flying Rabbit AIPA",
+            "sub": "MONYO Brewing Co.",
+            "text": "IPA - American"
+        },
+        {
+            "title": "Liquid Cocaine",
+            "sub": "Mad Scientist Beer",
+            "text": "IPA - Imperial"
+        },
+        {
+            "title": "Organic Chocolate Stout",
+            "sub": "Samuel Smith Old Brewery",
+            "text": "Stout - English"
+        },
+        {
+            "title": "Bracia",
+            "sub": "Thornbridge Brewery",
+            "text": "Strong Ale - English"
+        },
+        {
+            "title": "Oatmeal Stout",
+            "sub": "Samuel Smith Old Brewery",
+            "text": "Stout - Oatmeal"
+        },
+        {
+            "title": "Black Tokyo Horizon",
+            "sub": "BrewDog",
+            "text": "Stout - American Imperial"
+        },
+        {
+            "title": "Vintage Ale",
+            "sub": "Fuller's",
+            "text": "Old Ale"
+        },
+        {
+            "title": "All the Leaves are Brown",
+            "sub": "Tempest Brewing Company",
+            "text": "Brown Ale - American"
+        }
+    ]
 }
 
 //kapcsoszárojel, egy objectumból kivonjuk a kulcsokat  (object destructuring)
@@ -36,7 +90,7 @@ const beerComponent = ({title, sub, text}) => {
 const loadEvent = async () => {
     const bodyElement = document.querySelector('body');
     const rootElement = document.getElementById("root");
-    const result = await parseJSON("/beers")
+    const result = beers
     
     bodyElement.insertAdjacentHTML('afterbegin', plusComponentForDesign())
 
